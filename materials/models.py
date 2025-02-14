5d7ec317d0f6c996f873b4d7d3e43eee208503bd
Svetlana Bazhenova <varta.73@mail.ru>
2025-02-14 19:46:46 +0300

    Инициализация проекта

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Введите название курса",
+    )
+    description = models.TextField(
+        verbose_name="Описание курса",
+        help_text="Введите описание курса",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="static/images",
+        verbose_name="Изображение",
+        help_text="Загрузите фото курса",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Введите название урока",
+    )
+    description = models.TextField(
+        verbose_name="Описание урока",
+        help_text="Введите описание урока",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="static/images",
+        verbose_name="Изображение",
+        help_text="Загрузите фото урока",
+        blank=True,
+        null=True,
+    )
+    course = models.ForeignKey(
+        Course, verbose_name="Курс", on_delete=models.CASCADE, related_name="category"
+    )
+    video_link = models.URLField(
+        verbose_name="Ссылка на видео",
+        help_text="Укажите ссылку на видео",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- users/models.py
+++ users/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.fields import CountryField
+from django_countries.fields import CountryField
 
 
 class User(AbstractUser):
     username = models.CharField(max_length=50, blank=True, null=True)
-    email = models.EmailField(unique=True, verbose_name='Email', help_text='Укажите Вашу почту')
-    avatar = models.ImageField(upload_to='users/avatar/', verbose_name='Аватар', blank=True, null=True, help_text='Загрузите свой аватар')
-    phone = models.CharField(max_length=12, verbose_name='Телефон', blank=True, null=True, help_text='Введите номер телефона')
-    country = CountryField(max_length=100, verbose_name='Страна')
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Укажите Вашу почту"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar/",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите свой аватар",
+    )
+    phone = models.CharField(
+        max_length=12,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    country = CountryField(max_length=100, verbose_name="Страна")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
